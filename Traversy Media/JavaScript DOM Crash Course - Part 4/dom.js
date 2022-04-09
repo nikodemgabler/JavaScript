9630baf1e399a3198b503d7555bc11f6485e6885
nikodemgabler <kodemkabler@gmail.com>
2022-04-09 11:23:17 +0200

    finished part4

--- Traversy Media/JavaScript DOM Crash Course - Part 4/dom.js
+++ Traversy Media/JavaScript DOM Crash Course - Part 4/dom.js
@@ -1,11 +1,12 @@
 let form = document.getElementById('addForm');
 let itemList = document.getElementById('items');
-
+let filter = document.querySelector('#search')
 
 
 form.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem)
 
+filter.addEventListener('keyup', filterItems)
 
 
 function addItem(e) {
@@ -40,13 +41,26 @@ function removeItem(e) {
     if(e.target.classList.contains('delete')) {
         let removingItem = e.target.parentNode;
         itemList.removeChild(removingItem);
-
     }
-    
 }
 
 
+function filterItems(e) {
 
+    let filterText = e.target.value.toLowerCase();
+    
+    let items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+        let itemName = item.firstChild.textContent;
+
+        if (itemName.toLowerCase().indexOf(filterText) != -1) {
+            item.style.display = "flex";
+        } else {
+            item.style.display = "none";
+        }
+    })
+}
 
 
 
