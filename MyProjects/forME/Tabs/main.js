481ee37de09b4068914555e4a78d467635cf4f0a
nikodemgabler <kodemkabler@gmail.com>
2022-04-22 07:09:54 +0200

    tabs

---
+++ MyProjects/forME/Tabs/main.js
@@ -0,0 +1,35 @@
+const bookmark = document.querySelectorAll('.bookmark');
+const bookmarks = document.querySelector('.bookmarks')
+
+
+
+
+for(i=0; i < bookmark.length; i++){
+
+    bookmark[i].addEventListener('click', function(e){
+
+        
+        
+    })
+}
+
+
+
+
+
+
+// bookmark.forEach(function(item){
+
+//     item.addEventListener('click', function(e){
+
+//         item.classList.add('active');
+
+//         console.log(item[0])
+//         // if(item.classList.contains('active')){
+//         //     bookmarks.classList.add('active');
+//         // }
+
+//     })
+
+
+// })
