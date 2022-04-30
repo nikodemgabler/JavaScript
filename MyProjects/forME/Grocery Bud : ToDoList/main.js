f54c1f3f78bf0c81077528c9e04622b04c5fa2f3
nikodemgabler <kodemkabler@gmail.com>
2022-04-30 15:24:43 +0200

    adding some js

--- MyProjects/forME/Grocery Bud : ToDoList/main.js
+++ MyProjects/forME/Grocery Bud : ToDoList/main.js
@@ -17,13 +17,56 @@ let editID = "";
 form.addEventListener('submit', addItem);
 
 // ********* FUNCTIONS *********
+// addItem
 function addItem(e) {
     e.preventDefault();
     const value = grocery.value;
-    const id = new Date().getTime().toString(); // we won't use it in professional projects
+    const id = new Date().getTime().toString(); // we won't use it in professional projects - zmienna nadajaca unikalne ID
 
-    // Mozemy dodac do listy
-    // Mozemy 
-    if(value)
 
+    if(value) {
+        console.log("value is truthy")
+    } else {
+        console.log("value is falsy")
+    }
+
+    // Submitujac button mamy 3 mozliwosci:
+    // 1. Dodanie itemu do listy - jezeli nie edytujemy 
+    if (value && !editFlag) {
+        const element = document.createElement('article');
+        element.classList.add('grocery-item');
+        // add id
+        const attr = document.createAttribute('data-id');
+        attr.value = id;
+        element.setAttributeNode(attr);
+        console.log(element)
+    }
+    // 2. Edytowanie itemu
+    else if(value && editFlag) {
+        console.log('editing');
+    }
+    // 3. Puste pole
+    else {
+        displayAlert("please enter value", "danger")
+        }
 }
+
+// displayAlert
+function displayAlert(text, action) { // text = to co chcemy pokazac // action = jaki będzie kolor (danger or sucess)
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+
+    // remove alert
+    setTimeout(function() {
+        alert.textContent = '';
+        alert.classList.remove(`alert-${action}`);
+    }, 1700)
+}
+
+
+// ********* LOCAL STORAGE *********
+
+
+
+
+// 6:49
